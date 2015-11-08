747459f2958b96e43e764476de7b904b3efe691c
knitomi90@inf.elte.hu <knitomi90@inf.elte.hu>
2015-11-08 12:37:19 +0000

    tesztek

---
+++ test/usermodel.test.js
@@ -0,0 +1,141 @@
+var expect = require("chai").expect;
+var bcrypt = require('bcryptjs');
+var Waterline = require('waterline');
+var waterlineConfig = require('../config/waterline');
+var userCollection = require('../models/user');
+var subjectCollection = require('../models/subject');
+var csatCollection = require('../models/csat');
+
+var User;
+
+before(function (done) {
+    // ORM indítása
+    var orm = new Waterline();
+
+    orm.loadCollection(Waterline.Collection.extend(userCollection));
+    orm.loadCollection(Waterline.Collection.extend(subjectCollection));
+    orm.loadCollection(Waterline.Collection.extend(csatCollection));
+    waterlineConfig.connections.default.adapter = 'memory';
+
+    orm.initialize(waterlineConfig, function(err, models) {
+        if(err) throw err;
+        User = models.collections.user;
+        done();
+    });
+});
+
+describe('UserModel', function () {
+   
+   beforeEach(function (done) {
+        User.destroy({}, function (err) {
+            if(err){console.log(err);}
+            done();
+        });
+    });
+    
+    it('should work', function () {
+        expect(true).to.be.true;
+    });
+    
+    it('should be able to create a user', function () {
+        return User.create({
+                mtra: 'TANART',
+                password: 'jelszo',
+                surname: 'Gipsz',
+                forename: 'Jakab',
+                role: 'teacher',
+        })
+        .then(function (user) {
+            expect(user.mtra).to.equal('TANART');
+            expect(bcrypt.compareSync('jelszo', user.password)).to.be.true;
+            expect(user.surname).to.equal('Gipsz');
+            expect(user.forename).to.equal('Jakab');
+            expect(user.role).to.equal('teacher');
+        });
+    });
+    
+    it('should be able to find a user', function() {
+        return User.create(getUserData())
+        .then(function(user) {
+            return User.findOneByMtra(user.mtra);
+        })
+        .then(function (user) {
+            expect(user.mtra).to.equal('TANART');
+            expect(bcrypt.compareSync('jelszo', user.password)).to.be.true;
+            expect(user.surname).to.equal('Gipsz');
+            expect(user.forename).to.equal('Jakab');
+            expect(user.role).to.equal('teacher');
+        });
+    });
+    
+    it('should be able to update a user', function() {
+        var newName = {
+            surname : 'Gipszelt',
+            forename : 'József'
+        };
+        
+        return User.create(getUserData())
+        .then(function(user) {
+            var id = user.id;
+            return User.update(id, { surname: newName.surname, forename: newName.forename });
+        })
+        .then(function (userArray) {
+            var user = userArray.shift();
+            expect(user.mtra).to.equal('TANART');
+            expect(bcrypt.compareSync('jelszo', user.password)).to.be.true;
+            expect(user.surname).to.equal(newName.surname);
+            expect(user.forename).to.equal(newName.forename);
+            expect(user.role).to.equal('teacher');
+        });
+    });
+    
+    
+    it('should throw error for invalid data: avatar', function () {
+        var userData = getUserData();
+    
+        userData.role = 'rossz';
+        
+        expect(User.create(userData)).to.throw;
+    });
+    
+    [
+        {name: 'surname', value: ''},
+        {name: 'forename', value: ''},
+        {name: 'mtra', value: ''},
+        {name: 'password', value: ''},
+        {name: 'role', value: ''},
+    ].forEach(function (attr) {
+        it('should throw error for invalid data: ' + attr.name, function () {
+            var userData = getUserData();
+    
+            userData[attr.name] = attr.value;
+            
+            expect(User.create(userData)).to.throw;
+        });    
+    });
+    
+    describe('#validPassword', function() {
+        it('should return true with right password', function() {
+             return User.create(getUserData()).then(function(user) {
+                 expect(user.validPassword('jelszo')).to.be.true;
+             })
+        });
+        it('should return false with wrong password', function() {
+             return User.create(getUserData()).then(function(user) {
+                 expect(user.validPassword('titkos')).to.be.false;
+             })
+        });
+    });
+    
+    
+});
+
+function getUserData() {
+    return {
+        mtra: 'TANART',
+        password: 'jelszo',
+        surname: 'Gipsz',
+        forename: 'Jakab',
+        role: 'teacher',
+    };
+}
---
+++ test/zombie.test.js
@@ -0,0 +1,93 @@
+var Browser = require('zombie');
+
+Browser.localhost(process.env.IP, process.env.PORT);
+
+describe('User visits index page', function() {
+    var browser = new Browser();
+    
+    before(function() {
+        return browser.visit('/');
+    });
+    
+    it('should be successful', function() {
+        browser.assert.success();
+    });
+    
+    it('should see welcome page', function() {
+        browser.assert.text('div.page-header > h1', 'Bejelentkezés');
+    });
+    
+    
+    
+});
+describe('User visits new subject page', function (argument) {
+        
+    var browser = new Browser();
+        
+    before(function() {
+        return browser.visit('/subjects/new');
+    });
+        
+    it('should go to the authentication page', function () {
+        browser.assert.redirected();
+        browser.assert.success();
+        browser.assert.url({ pathname: '/login' });
+    });
+        
+    it('should be able to login with correct credentials', function (done) {
+        browser
+            .fill('mtra', 'TANART')
+            .fill('password', 'jelszo')
+            .pressButton('button[type=submit]')
+            .then(function () {
+                browser.assert.redirected();
+                browser.assert.success();
+                browser.assert.url({ pathname: '/subjects/list' });
+                done();
+        });
+    });
+        
+    it('should go the subjects page', function () {
+        return browser.visit('/subjects/new')
+        .then(function () {
+            browser.assert.success();
+            browser.assert.text('div.page-header > h1', 'Tárgy létrehozása');
+        });
+    });
+    
+    it('should show errors if the form fields are not right', function () {
+        return browser
+            .fill('subjectName', '')
+            .fill('description', '')
+            .fill('room', '')
+            .pressButton('button[type=submit]')
+            .then(function() {
+                // browser.assert.redirected();
+                browser.assert.success();
+                browser.assert.element('form .form-group:nth-child(1) [name=subjectName]');
+                browser.assert.hasClass('form .form-group:nth-child(1)', 'has-error');
+                browser.assert.element('form .form-group:nth-child(2) [name=description]');
+                browser.assert.hasClass('form .form-group:nth-child(2)', 'has-error');
+                browser.assert.element('form .form-group:nth-child(3) [name=room]');
+                browser.assert.hasClass('form .form-group:nth-child(3)', 'has-error');
+            });
+    });
+    
+    it('should show submit the right-filled form fields and go back to list page', function() {
+        browser
+            .fill('subjectName', 'Teszt tárgy')
+            .fill('description', 'Teszt tárgy leírás')
+            .fill('room', 'Teszt tárgy terem')
+            .pressButton('button[type=submit]')
+            .then(function() {
+                browser.assert.redirected();
+                browser.assert.success();
+                browser.assert.url({ pathname: '/subjects/list' });
+                
+                browser.assert.element('table.table');
+                browser.assert.text('table.table tbody tr:last-child td:nth-child(2)', 'Teszt tárgy');    
+                browser.assert.text('table.table tbody tr:last-child td:nth-child(3)', 'Teszt tárgy terem');    
+                browser.assert.text('table.table tbody tr:last-child td:nth-child(4)', 'Teszt tárgy leírás');
+            });
+    });
+});
